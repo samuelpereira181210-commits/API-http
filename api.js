45605474cc7dcc6cc971603090abead4300d517f
SAS <samuel.pereira181210@gmail.com>
2026-08-24 21:26:54 +0000

    API criada e funcional

---
+++ api.js
@@ -0,0 +1,58 @@
+import http from 'node:http'
+
+const porta = 3000
+
+const tarefas = [
+    {id: 1, titulo: 'Lavar Louças'},
+    {id: 2, titulo: 'Comprar uma RTX 5090'}
+]
+
+const server = http.createServer((requisicao, resposta) => {
+    resposta.setHeader('content-type','application/json; charset=utf-8')
+
+    if(requisicao.method == 'GET' && requisicao.url == '/tarefas'){
+        resposta.statusCode = 200
+        resposta.end(JSON.stringify(tarefas)) //JSON.stringfy trasforma JSON em string
+    }
+    else if(requisicao.method == 'GET' && requisicao.url == '/tarefa'){
+        let body = ''
+
+        requisicao.on('data', (chunk) => {
+            body += chunk.toString()
+        })
+
+        requisicao.on('end', () => {
+            try{
+                const novaTarefa = JSON.parse(body) //JSON.parse tranforma string em JSON
+
+                if(!novaTarefa.titulo){
+                    resposta.statusCode = 400 //Erro de cliente 
+                    resposta.end(JSON.stringify({error: 'O campo "titulo" é obrigadorio.'}))
+                }
+
+                const tarefaCriada = {
+                    id: tarefas.length + 1,
+                    titulo: novaTarefa.titulo
+                }
+
+                tarefas.push(tarefaCriada)
+
+                resposta.statusCode = 201 //Recuso Criado
+                resposta.end(JSON.stringify(tarefaCriada))
+
+
+            } catch(error) {
+                resposta.statusCode = 400
+                resposta.end(JSON.stringify ({error: 'Formato JSON invalido!'}))
+            }
+        })
+    } else {
+        resposta.statusCode = 404
+        resposta.end(JSON.stringify({error: 'Página não encontrada.'}))
+    }
+});
+
+server.listen(porta, () => {
+    console.log(`Srvidor funcionando na porta ${porta}`);
+})
+
